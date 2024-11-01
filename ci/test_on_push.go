350458afb9ce50df1803cd2335f4b133914eb779
Slug-Boi <github.unshipped609@slmail.me>
2024-11-01 08:34:05 +0100

    ci: update test on push src path

--- ci/test_on_push.go
+++ ci/test_on_push.go
@@ -25,17 +25,17 @@ func main() {
 	// at /src in the container
 	source := client.Container().
 		From("golang:1.23").
-		WithDirectory("/src", client.Host().Directory(".", dagger.HostDirectoryOpts{
+		WithDirectory("/src_d", client.Host().Directory(".", dagger.HostDirectoryOpts{
 			Exclude: []string{"build/"},
-		})).WithMountedCache("/src/dagger_dep_cache/go_dep", goCache)
+		})).WithMountedCache("/src_d/dagger_dep_cache/go_dep", goCache)
 
 		// set the working directory in the container
 		// install application dependencies
-	runner := source.WithWorkdir("/src/src_code/go_src/").
+	runner := source.WithWorkdir("/src_d/src").
 		WithExec([]string{"go", "mod", "tidy"}).WithEnvVariable("CI", "true")
 
 		// run application tests
-	out, err := runner.WithWorkdir("/src/src_code/go_src/").WithExec([]string{"go", "test", "./..."}).
+	out, err := runner.WithWorkdir("/src_d/src").WithExec([]string{"go", "test", "./..."}).
 		Stderr(ctx)
 	if err != nil {
 		panic(err)
