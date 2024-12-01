495606d798b7fcf71c591dfcd69c441c1829ed97
Slug-Boi <github.unshipped609@slmail.me>
2024-12-01 22:18:43 +0100

    chore: refactor install scripts to be actual scripts and simplify approach

--- installer/install.ps1
+++ installer/install.ps1
@@ -1,8 +1,63 @@
-$binaryUrl = "https://github.com/Slug-Boi/cocommit/blob/main/installer/bin/install-win"
-$outputPath = "install-win.exe"
+# Determine the OS and architecture
 
-# Download the binary
-Invoke-WebRequest -Uri $binaryUrl -OutFile $outputPath
+# Set up a cleanup function to be triggered upon script exit
+function Cleanup {
+    Remove-Item -ErrorAction SilentlyContinue "cocommit.tar.gz"
+    Remove-Item -ErrorAction SilentlyContinue "author.txt"
+    if ($file) {
+        Remove-Item -ErrorAction SilentlyContinue $file
+    }
+}
 
-# Run the binary
-& .\$outputPath
+trap { Cleanup } EXIT
+
+$OS = (Get-CimInstance Win32_OperatingSystem).Caption
+$ARCH = (Get-CimInstance Win32_Processor).Architecture
+
+$file = ""
+
+$url = "https://github.com/Slug-Boi/cocommit/releases/latest/download/"
+
+# Set the download URL based on the OS and architecture
+if ($OS -match "Windows") {
+    $URL = "${url}cocommit-win.tar.gz"
+    $file = "cocommit.exe"
+} else {
+    Write-Host "Unsupported OS: $OS"
+    exit 1
+}
+
+# Download and run the script
+Invoke-WebRequest -Uri $URL -OutFile "cocommit.tar.gz"
+if ($?) {
+    tar -xvzf "cocommit.tar.gz"
+    Remove-Item "cocommit.tar.gz"
+    Remove-Item "author.txt"
+    if ($file) {
+        & .\$file -v
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host "Failed to extract the binary"
+            exit 1
+        }
+    }
+} else {
+    Write-Host "Failed to download the file"
+    exit 1
+}
+
+# Move the binary to the specified directory
+$target_dir = Read-Host "Enter the directory to move the binary to (default: C:\Program Files\cocommit)"
+$target_dir = if ($target_dir) { $target_dir } else { "C:\Program Files\cocommit" }
+
+if (-Not (Test-Path (Split-Path $target_dir))) {
+    Write-Host "Directory does not exist: $(Split-Path $target_dir)"
+    exit 1
+}
+
+Move-Item -Path $file -Destination $target_dir
+if ($?) {
+    Write-Host "Binary moved to $target_dir successfully"
+} else {
+    Write-Host "Failed to move the binary to $target_dir"
+    exit 1
+}
--- installer/install.sh
+++ installer/install.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 # Determine the OS and architecture
+
+# Set up a cleanup function to be triggered upon script exit
+__cleanup ()
+{
+    rm "cocommit.tar.gz" 2>/dev/null
+    rm author.txt 2>/dev/null
+    if [ -n "$file" ]; then
+        rm $file 2>/dev/null
+    fi
+}
+
+trap __cleanup EXIT
+
 OS=$(uname -s)
 ARCH=$(uname -m)
 
 file=""
 
-url="https://github.com/Slug-Boi/cocommit/raw/refs/heads/chore_install_script/installer/bin/"
+url="https://github.com/Slug-Boi/cocommit/releases/latest/download/"
 
 # Set the download URL based on the OS and architecture
 if [ "$OS" == "Linux" ]; then
-    URL="${url}install-linux"
-    file="install-linux"
+    URL="${url}cocommit-linux.tar.gz"
+    file="cocommit-linux"
 elif [ "$OS" == "Darwin" ]; then
     if [ "$ARCH" == "x86_64" ]; then
-        URL="${url}install-darwin-x86_64"
-        file="install-darwin-x86_64"
+        URL="${url}cocommit-darwin-x86_64.tar.gz"
+        file="cocommit-darwin"
     else
-        URL="${url}install-darwin-aarch64"
-        file="install-darwin-aarch64"
+        URL="${url}cocommit-darwin-aarch64.tar.gz"
+        file="cocommit-darwin-aarch64"
     fi
 else
     echo "Unsupported OS: $OS"
@@ -26,5 +39,29 @@ fi
 
 # Download and run the script
 
-echo $file
-curl -LJO $URL && chmod +x $file && ./$file
+curl -L -o cocommit.tar.gz $URL && \
+tar -xvzf cocommit.tar.gz && \
+rm cocommit.tar.gz && rm author.txt && \
+chmod +x $file && ./$file -v
+if [ $? -ne 0 ]; then
+    echo "Failed to extract the binary"
+    exit 1
+fi
+
+# Move the binary to the current directory
+read -p "Enter the directory to move the binary to (default: /usr/local/bin/cocommit): " target_dir
+target_dir=${target_dir:-/usr/local/bin/cocommit}
+
+if [ ! -d "$(dirname "$target_dir")" ]; then
+    echo "Directory does not exist: $(dirname "$target_dir")"
+    exit 1
+fi
+
+mv $file "$target_dir"
+if [ $? -ne 0 ]; then
+    echo "Failed to move the binary to $target_dir"
+    exit 1
+fi
+
+echo "Binary moved to $target_dir successfully"
+
