a84e38d96ef1517ef694597c66e7be22e7a9a382
Slug-Boi <theis.p.holm@gmail.com>
2025-04-17 23:31:51 +0200

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    prog_src = {
+      flake = false;
+      url = "github:Slug-Boi/cocommit";
+      rev = "v1.3.0";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: let
+    forAllSys = flake-utils.lib.eachSystem flake-utils.lib.allSystems;
+
+    APPNAME = "cocommit";
+    appOverlay = final: prev: {
+      # any pkgs overrides made here will be
+      # inherited in the arguments of default.nix
+      # because we used final.callPackage instead of prev.callPackage
+      # i.e.
+      # nodejs = prev.nodejs.overrideAttrs { name = "stinky"; };
+      # would make it so that final.callPackage gives the altered nodejs
+      ${APPNAME} = final.callPackage ./. { inherit (inputs) prog_src; };
+    };
+  in {
+    overlays.default = appOverlay;
+  } // (
+    forAllSys (system: let
+      pkgs = import nixpkgs { inherit system; overlays = [ appOverlay ]; };
+    in{
+      packages = {
+        default = pkgs.${APPNAME};
+      };
+    })
+  );
+}
